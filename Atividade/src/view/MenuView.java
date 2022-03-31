c557163365d62423abf805b6cbb53cfb6fb6ce89
Pancieri <101364528+TPancieri@users.noreply.github.com>
2022-03-31 17:15:25 -0300

    Add files via upload

---
+++ Atividade/src/view/MenuView.java
@@ -0,0 +1,36 @@
+package view;
+
+import java.util.Scanner;
+
+public class MenuView {
+
+	public void menu() {
+
+		System.out.println("***  MENU  ***");
+		System.out.println("-");
+		System.out.println("-1.Cadastrar Usuario");
+		System.out.println("-2.Cadastrar TipoVeiculo");
+		System.out.println("-3.Cadastrar Veiculo");
+		System.out.println("-4.Cadastrar Pessoa");
+		System.out.println("-9.Voltar");
+
+		Scanner input = new Scanner(System.in);
+
+		int number = input.nextInt();
+		switch (number) {
+		case 1:
+			UsuarioView user = new UsuarioView();
+			user.menuUsuario(null);
+			break;
+		case 9:
+
+			LoginView login = new LoginView();
+			login.login();
+			MenuView menuGeral = new MenuView();
+			menuGeral.menu();
+			break;
+		}
+
+	}
+
+}
